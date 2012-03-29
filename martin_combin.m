c2ef1ddb8bc2f57ebe3dc26e8206e27cc625aca7
Martin Schorb <martin.schorb@embl.de>
2012-03-29 16:12:56 +0200

    bugfix combin

--- martin_combin.m
+++ martin_combin.m
@@ -9,6 +9,8 @@ function out = martin_combin(n,k)
 
 if n == k
    out=1:n;
+elseif k==1;
+   out=(1:n)';  
 elseif n == k + 1
    tmp =1:n;
    out = tmp(ones(n,1),:);
@@ -21,13 +23,14 @@ else
       for ii=1:n
         binary=[repmat(0,[a/(2^ii) 1]);repmat(1,[a/(2^ii) 1])];
         sel(:,ii)=repmat(binary,[2^(ii-1) 1]);
-      end           
-end
+      end
 
    idx = sel(sum(sel,2) == k,:);
    nrows = size(idx,1);
    [rows,xxx] = find(idx');
-   
-   vec=1:n;
-   out = reshape(vec(rows),k,nrows).';
 
+   out = flipud(reshape(rows,k,nrows).');
+          
+end
+
+   
